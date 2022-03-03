0a728ff0db776651eb4d40b68a539e4d30f412d0
JIVESH ARYA <jivesh.arya@gmail.com>
2022-03-03 14:31:37 +0530

    done

--- server.js
+++ server.js
@@ -14,5 +14,5 @@ if (process.env.NODE_ENV === "production") {
 }
 
 const PORT = process.env.PORT || 5000;
-
+console.log("starting to listen");
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
